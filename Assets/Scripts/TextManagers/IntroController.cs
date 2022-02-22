abeeed42838d5f5fd4298f6b57a5e60f9d29b98d
Catherine Martin <cmartin@grovergaming.com>
2022-02-21 21:17:23 -0500

    Text Writer Set-Up
    Initial Set Up of Text Writer, need to actually get working and polish

---
+++ Assets/Scripts/TextManagers/IntroController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IntroController : MonoBehaviour {
+
+	public Text TextObject;
+
+	private string[] _intro = new string[]
+	{
+		"Somewhere in a forest lives a little raccoon dog named Tan Chuki.",
+		"A raccoon dog’s life is lots of fun!  They play with magic, and eat lots of nuts and berries.",
+		"Recently Tan Chuki has heard of this thing called “money.” Other animals use it to buy lots of stuff!",
+		"Tan Chuki wants lots of stuff too. He wants to throw a whole party with his other raccoon dog friends!",
+		"So, Tan Chuki goes and opens up a small shop in a little town…",
+		"But what do raccoon dogs sell?",
+		"“Ah!” thinks Tan Chuki. “I will use my magic to create things for people!”",
+		"“I just need to make enough money in three days…”",
+		"“...Before they notice I have made their riches with leaves and mud!”",
+		"And so, the con of Tan Chuki begins….",
+	};
+
+	private int _lineIndex;
+
+	void Start()
+    {
+		_lineIndex = 0;
+    }
+
+	void PlayIntro()
+    {
+		TextWriter.Instance.WriteLine(TextObject, _intro[_lineIndex]);
+    }
+	
+	
+}
---
+++ Assets/Scripts/TextManagers/TextWriter.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class TextWriter : Singleton<TextWriter> {
+
+	private Sequence _typeWriter;
+	private List<Sequence> _key;
+	//create queue
+
+    public void WriteLine(Text txt, string line)
+    {
+        if (!_typeWriter.IsPlaying())
+        {
+			_typeWriter = DOTween.Sequence();
+			_key = new List<Sequence>();
+			txt.text = "";
+			string passIn = "";
+			char[] toWrite = line.ToCharArray();
+			
+			for(int i = 0; i < toWrite.Length; i++)
+            {
+				_key.Add(TypeWriter(passIn, toWrite[i]));
+            }
+			
+		}
+    }
+
+	private Sequence TypeWriter(string t, char c)
+    {
+		Sequence seq = DOTween.Sequence().AppendInterval(0.125f).AppendCallback(() => 
+		{
+			t += c;
+		});
+
+		return seq;
+    }
+}
