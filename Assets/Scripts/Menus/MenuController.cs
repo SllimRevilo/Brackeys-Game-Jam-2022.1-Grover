635afc3d496c624489827912cde95d83c5ff47fc
Catherine Martin <cmartin@grovergaming.com>
2022-02-26 16:46:23 -0500

    Main Menu Setup
    Basic Main Menu Setup

---
+++ Assets/Scripts/Menus/MenuController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour {
+
+	public GameObject Menu;
+	public GameObject CreditsMenu;
+	public Text Score;
+
+	private const string SCOR = "score";
+	private const string HIGH = "High Score: ";
+
+	// Use this for initialization
+	void Start () {
+		CreditsMenu.SetActive(false);
+		Score.text = HIGH + PlayerPrefs.GetFloat(SCOR, 0);
+	}
+
+	public void NewGame()
+    {
+		Menu.SetActive(false);
+    }
+
+	public void ClearProgress()
+    {
+		PlayerPrefs.SetFloat(SCOR, 0);
+		Score.text = HIGH + PlayerPrefs.GetFloat(SCOR, 0);
+	}
+
+	public void Quit()
+    {
+		Application.Quit();
+    }
+
+	public void Credits()
+    {
+		Menu.SetActive(false);
+		CreditsMenu.SetActive(true);
+    }
+
+	public void MainMenu()
+    {
+		Menu.SetActive(true);
+		CreditsMenu.SetActive(false);
+    }
+	
+}
